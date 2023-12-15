3a7a9b33417e92b9d991c89e1d3e673b9c39b331
murali <murali.yagala@alongx.com>
2023-12-15 13:29:43 +0530

    fixing error-2

--- GSM_Webapp/wsgi.py
+++ GSM_Webapp/wsgi.py
@@ -1,7 +1,8 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GSM_Webapp.settings')
+settings_module = 'GSM_Webapp.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'GSM_Webapp.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
