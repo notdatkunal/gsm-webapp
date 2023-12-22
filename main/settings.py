b7de5953558356850da05b38c3cf85d3cb398b7c
murali <murali.yagala@alongx.com>
2023-12-22 20:32:42 +0530

    push 7

--- main/settings.py
+++ main/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+        'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
     'app'
 ]
 
