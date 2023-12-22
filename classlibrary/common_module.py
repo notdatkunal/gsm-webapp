96486af79e9f3f7aa0889324ea06944279ce936c
murali <murali.yagala@alongx.com>
2023-12-22 19:45:56 +0530

    push 4

--- classlibrary/common_module.py
+++ classlibrary/common_module.py
@@ -1,21 +1,9 @@
 import requests
 from django.http import HttpResponse
-import os
-from io import BytesIO
-from azure.storage.blob import BlobServiceClient
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient,BlobSasPermissions,generate_blob_sas
-
-def azure_connection():
-    # from env take AZURE_STORAGE_CONNECTION_STRING
-    connect_string = os.environ['AZURE_STORAGE_CONNECTION_STRING']
-    # createing connecction for azure blob
-    blob_service_client = BlobServiceClient.from_connection_string(connect_string)
-    # return blob 
-    return blob_service_client
 
 class Data:
     def __init__(self,api_link) -> None:
