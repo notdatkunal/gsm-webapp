aeed86d6288136c39a0816873679102b34d6cdec
Murali Yagala <murali.yagala@alongx.com>
2023-12-22 12:54:43 +0000

    settings.py edited online with Bitbucket

--- main/settings.py
+++ main/settings.py
@@ -94,6 +94,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 # STATIC_URL = '/assets/'
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'assets')
 
